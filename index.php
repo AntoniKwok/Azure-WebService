c83b9576f5cdd3e1ad74b3f399fd8c4058954bf1
Antoni Wijaya <antoniwijaya.kwok@gmail.com>
2019-07-12 14:41:05 +0700

    Add Design Using Bulma

--- index.php
+++ index.php
@@ -1,86 +1,106 @@
-<html>
+ <!DOCTYPE html>
+ <html lang="en">
  <head>
- <Title>Registration Form</Title>
- <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
- 	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <Title>Registration Form</Title>
+     <style type="text/css">
+        body { background-color: #fff; border-top: solid 10px #000;
+            color: #333; font-size: .85em; margin: 20; padding: 20;
+            font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+        }
+        h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+        h1 { font-size: 2em; }
+        h2 { font-size: 1.75em; }
+        h3 { font-size: 1.2em; }
+        table { margin-top: 0.75em; }
+        th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+        td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+    </style>
+    <link rel="stylesheet" href="/bulma/css/bulma.min.css">
  </head>
  <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/><br><br>
-       Email <input type="text" name="email" id="email"/><br><br>
-       Job <input type="text" name="job" id="job"/><br><br>
-       <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
- </form>
- <?php
-    $host = "antoni.database.windows.net";
-    $user = "Antoni";
-    $pass = "Wijaya123";
-    $db = "kwok";
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    }
-    if (isset($_POST['submit'])) {
+    <h1>Register here!</h1>
+    <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+    <form method="post" action="index.php" enctype="multipart/form-data" >
+        <div class="control has-icons-left has-icons-right">
+            <input class="input" type="text" name="name" id="name" placeholder="Name">
+            <span class="icon is-small is-left">
+                <i class="fas fa-address-card"></i>
+            </span>
+        </div>
+        <div class="control has-icons-left has-icons-right">
+            <input class="input" type="email" name="email" id="email" placeholder="Email">
+            <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+            </span>
+        </div>
+        <div class="control has-icons-left has-icons-right">
+            <input class="input"type="text" name="job" id="job" placeholder="Job">
+            <span class="icon is-small is-left">
+                <i class="fas fa-briefcase"></i>
+            </span>
+        </div>
+        <input type="submit" name="submit" value="Submit" />
+        <input type="submit" name="load_data" value="Load Data" />
+    </form>
+    <?php
+        $host = "antoni.database.windows.net";
+        $user = "Antoni";
+        $pass = "Wijaya123";
+        $db = "kwok";
         try {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $job = $_POST['job'];
-            $date = date("Y-m-d");
-            // Insert data
-            $sql_insert = "INSERT INTO Users (name, email, job, date) 
-                        VALUES (?,?,?,?)";
-            $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $name);
-            $stmt->bindValue(2, $email);
-            $stmt->bindValue(3, $job);
-            $stmt->bindValue(4, $date);
-            $stmt->execute();
+            $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+            $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         } catch(Exception $e) {
             echo "Failed: " . $e;
         }
-        echo "<h3>Your're registered!</h3>";
-    } else if (isset($_POST['load_data'])) {
-        try {
-            $sql_select = "SELECT * FROM Users";
-            $stmt = $conn->query($sql_select);
-            $registrants = $stmt->fetchAll(); 
-            if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
-                echo "<table>";
-                echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
-                echo "<th>Date</th></tr>";
-                foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['name']."</td>";
-                    echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
-                    echo "<td>".$registrant['date']."</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<h3>No one is currently registered.</h3>";
+        if (isset($_POST['submit'])) {
+            try {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $job = $_POST['job'];
+                $date = date("Y-m-d");
+                // Insert data
+                $sql_insert = "INSERT INTO Users (name, email, job, date) 
+                            VALUES (?,?,?,?)";
+                $stmt = $conn->prepare($sql_insert);
+                $stmt->bindValue(1, $name);
+                $stmt->bindValue(2, $email);
+                $stmt->bindValue(3, $job);
+                $stmt->bindValue(4, $date);
+                $stmt->execute();
+            } catch(Exception $e) {
+                echo "Failed: " . $e;
             }
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
+            echo "<h3>Your're registered!</h3>";
+        } else if (isset($_POST['load_data'])) {
+            try {
+                $sql_select = "SELECT * FROM Users";
+                $stmt = $conn->query($sql_select);
+                $registrants = $stmt->fetchAll(); 
+                if(count($registrants) > 0) {
+                    echo "<h2>People who are registered:</h2>";
+                    echo "<table>";
+                    echo "<tr><th>Name</th>";
+                    echo "<th>Email</th>";
+                    echo "<th>Job</th>";
+                    echo "<th>Date</th></tr>";
+                    foreach($registrants as $registrant) {
+                        echo "<tr><td>".$registrant['name']."</td>";
+                        echo "<td>".$registrant['email']."</td>";
+                        echo "<td>".$registrant['job']."</td>";
+                        echo "<td>".$registrant['date']."</td></tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "<h3>No one is currently registered.</h3>";
+                }
+            } catch(Exception $e) {
+                echo "Failed: " . $e;
+            }
         }
-    }
- ?>
+    ?>
  </body>
  </html>
