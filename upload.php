b8a2af9878a11a8d5cae44936085c63c87c6dac0
Antoni Wijaya <antoniwijaya.kwok@gmail.com>
2019-07-27 15:34:22 +0700

    Testing v6 CP

--- upload.php
+++ upload.php
@@ -7,7 +7,7 @@
 
         $terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
 
-
+        
         if ($terupload) {
             echo "Upload berhasil!<br/>";
             echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
