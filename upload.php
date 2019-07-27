5b944fe29ad1060126eb8b1f71bf71e2e2b4ebde
britneyll <britneylavenda@gmail.com>
2019-07-27 09:53:57 +0700

    Upload Image TEsting v43

---
+++ upload.php
@@ -0,0 +1,20 @@
+<?php
+    if($isset($_POST['submit'])){
+        $namaFile = $_FILES['image']['name'];
+        $namaSementara = $_FILES['image']['tmp_name'];
+
+        $dirUpload = "images/";
+
+        $terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
+
+        if ($terupload) {
+            echo "Upload berhasil!<br/>";
+            echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+        } else {
+            echo "Upload Gagal!";
+        }
+        // $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+        // header("Location: analyze.php");
+    }
+?>
