29c6f4313d372414ccbd490ab467583b24b00623
Antoni Wijaya <antoniwijaya.kwok@gmail.com>
2019-07-27 19:23:33 +0700

    Test v5

--- upload.php
+++ upload.php
@@ -1,36 +1,21 @@
 <?php
     require_once "vendor/autoload.php"; 
 
-    use WindowsAzure\Common\ServicesBuilder;
-    use WindowsAzure\Common\ServiceException;
-    use WindowsAzure\Blob\Models\Block;
-    use WindowsAzure\Blob\Models\BlockList;
-    use WindowsAzure\Blob\Models\BlobBlockType;
-
+    
     if(isset($_POST['submit'])){
+        $namaFile = $_FILES['image']['name'];
+        $namaSementara = $_FILES['image']['tmp_name'];
+
+        $dirUpload = "";
+
+        $terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
         
-        $connectionString = "DefaultEndpointsProtocol=https://bluejack.azurewebsites.net;AccountName=".'kwoks'.";AccountKey=".'GNU7bWnM4Ws5Zcg/FZB7T0YtVEd+kTgZpODhoydogcSCefqmDua3Z+zby8jne6iSlve0GuemBhIhXQ7nzH6J6Q==';
-        $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-        $file_name = $argv[1];
-        $blob_name = basename($file_name);
-        $containerName = "blobs".$blob_name;
-        $block_list = new BlockList();
-        define('CHUNK_SIZE', 4 * 1024 * 1024);
-        try {
-            $fptr = fopen($file_name, "rb");
-            $index = 1;
-            while (!feof($fptr)) {
-                $block_id = base64_encode(str_pad($index, 6, "0", STR_PAD_LEFT));
-                $block_list->addUncommittedEntry($block_id);
-                $data = fread($fptr, CHUNK_SIZE);
-                $blobRestProxy->createBlobBlock($containerName, $blob_name, $block_id, $data);
-                ++$index;
-            }
-            $blobRestProxy->commitBlobBlocks($containerName, $blob_name, $block_list);
-        } catch (ServiceException $e) {
-            $code = $e->getCode();
-            $error_message = $e->getMessage();
-            echo $code.": ".$error_message."<br />";
+        if ($terupload) {
+            echo "Upload berhasil!<br/>";
+            echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+        } else {
+            echo "Upload Gagal!";
         }
     }
 ?>
