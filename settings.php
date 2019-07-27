6807752b063fb5d6a6a1874a22e13d6e0ee2c247
Antoni Wijaya <antoniwijaya.kwok@gmail.com>
2019-07-27 19:54:26 +0700

    Test v6

--- settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-$settings = array(
-    "container" => "kwoks",
-    "protocol" => "https://bluejack.azurewebsites.net",
-    "account_name" => "kwoks",
-    "account_key" => "GNU7bWnM4Ws5Zcg/FZB7T0YtVEd+kTgZpODhoydogcSCefqmDua3Z+zby8jne6iSlve0GuemBhIhXQ7nzH6J6Q=="
-);
-?>
--- upload.php
+++ upload.php
@@ -1,7 +1,12 @@
 <?php
     require_once "vendor/autoload.php"; 
+    use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+    use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+    use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+    use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+    use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-    
+
     if(isset($_POST['submit'])){
         $namaFile = $_FILES['image']['name'];
         $namaSementara = $_FILES['image']['tmp_name'];
