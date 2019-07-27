a004418d7529da60ac6327c4c8eb648ca94dbf80
Antoni Wijaya <antoniwijaya.kwok@gmail.com>
2019-07-27 19:18:36 +0700

    Test v3

--- upload.php
+++ upload.php
@@ -9,31 +9,26 @@
 
     if(isset($_POST['submit'])){
         $connectionString = "DefaultEndpointsProtocol=https://bluejack.azurewebsites.net;AccountName=".'kwoks'.";AccountKey=".'GNU7bWnM4Ws5Zcg/FZB7T0YtVEd+kTgZpODhoydogcSCefqmDua3Z+zby8jne6iSlve0GuemBhIhXQ7nzH6J6Q==';
-        $namaFile = $_FILES['image']['name'];
-        $namaSementara = $_FILES['image']['tmp_name'];
-        
-        $dirUpload = "";
-
-        $terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
-
-        $blobClient = BlobRestProxy::createBlobService($connectionString);
- 
-        # Membuat BlobService yang merepresentasikan Blob service untuk storage account
-        $createContainerOptions = new CreateContainerOptions();
-    
-        $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-
-        $containerName = "blobs".$namaFile;
-        
-        if ($terupload) {
-            echo "Upload berhasil!<br/>";
-            echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
-            $blobClient->createBlockBlob($containerName, $namaFile, $namaSementara);
-            $listBlobsOptions = new ListBlobsOptions();
-            $listBlobsOptions->setPrefix("");
-            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-        } else {
-            echo "Upload Gagal!";
+        $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+        $file_name = $argv[1];
+        $blob_name = basename($file_name);
+        $block_list = new BlockList();
+        define('CHUNK_SIZE', 4 * 1024 * 1024);
+        try {
+            $fptr = fopen($file_name, "rb");
+            $index = 1;
+            while (!feof($fptr)) {
+                $block_id = base64_encode(str_pad($index, 6, "0", STR_PAD_LEFT));
+                $block_list->addUncommittedEntry($block_id);
+                $data = fread($fptr, CHUNK_SIZE);
+                $blobRestProxy->createBlobBlock($settings["container"], $blob_name, $block_id, $data);
+                ++$index;
+            }
+            $blobRestProxy->commitBlobBlocks($settings["container"], $blob_name, $block_list);
+        } catch (ServiceException $e) {
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code.": ".$error_message."<br />";
         }
     }
 ?>
