2813f7af62b74f804a15d94a20aa1b1ae2d04c1e
Antoni Wijaya <antoniwijaya.kwok@gmail.com>
2019-07-27 19:21:46 +0700

    Test v4

--- upload.php
+++ upload.php
@@ -8,10 +8,12 @@
     use WindowsAzure\Blob\Models\BlobBlockType;
 
     if(isset($_POST['submit'])){
+        
         $connectionString = "DefaultEndpointsProtocol=https://bluejack.azurewebsites.net;AccountName=".'kwoks'.";AccountKey=".'GNU7bWnM4Ws5Zcg/FZB7T0YtVEd+kTgZpODhoydogcSCefqmDua3Z+zby8jne6iSlve0GuemBhIhXQ7nzH6J6Q==';
         $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
         $file_name = $argv[1];
         $blob_name = basename($file_name);
+        $containerName = "blobs".$blob_name;
         $block_list = new BlockList();
         define('CHUNK_SIZE', 4 * 1024 * 1024);
         try {
@@ -21,10 +23,10 @@
                 $block_id = base64_encode(str_pad($index, 6, "0", STR_PAD_LEFT));
                 $block_list->addUncommittedEntry($block_id);
                 $data = fread($fptr, CHUNK_SIZE);
-                $blobRestProxy->createBlobBlock($settings["container"], $blob_name, $block_id, $data);
+                $blobRestProxy->createBlobBlock($containerName, $blob_name, $block_id, $data);
                 ++$index;
             }
-            $blobRestProxy->commitBlobBlocks($settings["container"], $blob_name, $block_list);
+            $blobRestProxy->commitBlobBlocks($containerName, $blob_name, $block_list);
         } catch (ServiceException $e) {
             $code = $e->getCode();
             $error_message = $e->getMessage();
